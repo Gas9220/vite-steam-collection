bf676eb768bbc0d81da717445c2f0f9d15429f44
Gaspare <Gas9220@users.noreply.github.com>
2023-05-31 09:47:14 +0200

    Starter project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import * as bootstrap from 'bootstrap';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import './assets/scss/main.scss';
+
+createApp(App).mount('#app')
